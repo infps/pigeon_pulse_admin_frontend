cc6bde6b035a2bf0182e737abaf46571e71bab6d
Tushar Daiya <tushardaiya@outlook.com>
2025-08-13 15:37:30 +0530

    .next update

--- frontend/.next/server/server-reference-manifest.js
+++ frontend/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"1wuq1b5CXU/QpIVtrDsJtDU3/DHEyRvqXwBXu8ozOoA=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
--- frontend/.next/static/chunks/src_app_(main)_schemas_page_tsx_6dd7b74c._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_061e0f03._.js",
-  "static/chunks/38087_zod_v4_44e07b2a._.js",
-  "static/chunks/node_modules__pnpm_49f05b0f._.js"
-],
-    source: "dynamic"
-});
